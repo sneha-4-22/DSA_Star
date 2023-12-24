04904fda2fafa192e80328c4bfc2ebd69625e43f
sneha kumari <snehakumari64445@gmail.com>
2023-12-24 16:36:01 +0530

    sorting

---
+++ Loops/foree.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class foree {
+    public static void main(String[] args) {
+        int a=0;
+       // for(int i=0;i<a;i++) System.out.print(i);
+    //    while(a<10){
+    //     System.out.println(a);
+    //     a++;
+    // }
+    do{
+        System.out.println(a);
+        a++;
+    }while(a<10);
+    }
+}
---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,20 @@
+package Sorting;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr={8,7,6,5,2};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
