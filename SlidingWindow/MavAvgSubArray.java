4fc7a2684251ffaf6b90c30b15abcd7510b2b604
sneha kumari <snehakumari64445@gmail.com>
2024-01-05 16:45:32 +0530

    Slidingwindow-1

---
+++ SlidingWindow/MavAvgSubArray.java
@@ -0,0 +1,15 @@
+package SlidingWindow;
+
+public class MavAvgSubArray {
+    public double findMaxAverage(int[] nums, int k) {
+        int n=nums.length;
+        double ans=0;
+        double window=0;
+        for(int i=0;i<k;i++) window+=nums[i];
+        ans=window/k;
+        for(int right=k;right<nums.length;right++){
+            window+=nums[right]-nums[right-k];
+            ans=Math.max(ans,window/k);
+        }return ans;
+    }
+}
