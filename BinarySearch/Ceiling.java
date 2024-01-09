ad990068f11a19d1f2cd91165e4bb6433411dc0b
sneha kumari <snehakumari64445@gmail.com>
2024-01-09 10:38:56 +0530

    🎴

---
+++ BinarySearch/Ceiling.java
@@ -0,0 +1,13 @@
+package BinarySearch;
+
+public class Ceiling {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start=0;
+        int end=letters.length-1;
+        while(start<=end){
+            int mid= start +(end-start)/2;
+            if(target<letters[mid]) end=mid-1;
+            else start=mid+1;
+        }return letters[start%letters.length];
+    }
+}
