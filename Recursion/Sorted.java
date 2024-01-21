54156b1483a94e6d9b4aad6b97ce680dc6044511
sneha kumari <snehakumari64445@gmail.com>
2024-01-21 11:40:42 +0530

    bs rec

--- Recursion/Sorted.java
+++ Recursion/Sorted.java
@@ -27,4 +27,24 @@ public class Sorted {
        if(arr[idx]==target) list.add(idx);
        return findAllIndex(arr, target, idx+1, list);
     }
+    static int search(int[] arr,int target,int s,int e){
+        if(s>e){
+            return -1;
+        }
+        int m=s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if(arr[s]<=arr[m]){
+            if(target>=arr[s]&&target<=arr[m]){
+                return search(arr,target,s,m-1);
+            }else{
+               return search(arr,target,m+1,e);
+            }
+        }
+        if(target>=arr[m]&&target<=arr[e]){
+            return search(arr,target,m+1,e);
+        }
+        return search(arr,target,s,m-1);
+    }
 }
