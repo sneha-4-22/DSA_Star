b18ed247cc316eb0957f55e6d618b712f1cb5bb1
sneha kumari <snehakumari64445@gmail.com>
2024-01-21 17:13:25 +0530

    selection sort using rec

--- Recursion/Star.java
+++ Recursion/Star.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Star {
     public static void main(String[] args) {
         int[] arr={23,1,2,4,5,67,12,90,1,2,3};
-        bubble(arr, arr.length-1, 0);
+        // bubble(arr, arr.length-1, 0);
+        selection(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
+        
     }
     static void yo1(int r,int c){
         if(r==0){
@@ -46,6 +48,23 @@ public class Star {
         }else{
             
             bubble(arr, r-1, 0);
+        }
+    }
+    static void selection(int[] arr,int r,int c,int max){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[max]){
+                selection(arr, r, c+1, c);
+            }else{
+                selection(arr, r, c+1, max);
+            }
+        }else{
+            int temp=arr[max];
+            arr[max]=arr[r-1];
+            arr[r-1]=temp;
+            selection(arr, r-1, 0, 0);
         }
     }
 }
