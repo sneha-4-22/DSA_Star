da10e9c9d25e606c91a8bf4e270ef9cc2cb80421
sneha kumari <snehakumari64445@gmail.com>
2024-01-22 10:10:29 +0530

    merge sort and permutaion

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -41,4 +41,48 @@ public class MergeSort {
         return mix;
          
     }
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+        int mid = (s + e) / 2;
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
+    }
+
 }
