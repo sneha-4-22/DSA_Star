0805f4f9a9049e018a17dbb7a177554099c74a8f
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-01-23 22:18:04 +0530

    Update MaxLength.java

--- ARRAYS/MaxLength.java
+++ ARRAYS/MaxLength.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    private boolean helper(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            if (!set.add(ch))
+                return false;
+        }
+        return true;
+    }
+    public int maxLength(List<String> arr) {
+        List<String> list = new ArrayList<>();
+        list.add("");
+        int res = 0;
+        for (String word : arr) {
+            for (int i = 0; i < list.size(); i++) {
+                String newword = list.get(i) + word;
+                if (helper(newword)) {
+                    list.add(newword);
+                    res = Math.max(res, newword.length());
+                }
+            }
+        }
+        return res;
+    }
+}
