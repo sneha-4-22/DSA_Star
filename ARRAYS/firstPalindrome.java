70417ca8f9c3c1ba56d84dd8febfa551dbac0798
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-02-13 19:27:00 +0530

    Update firstPalindrome.java

--- ARRAYS/firstPalindrome.java
+++ ARRAYS/firstPalindrome.java
@@ -1,1 +1,19 @@
-
+class firstPalindrome {
+    public String Palindrome(String[] words) {
+        for(String str:words){
+            int s=0;
+            int l=str.length()-1;
+            boolean flag=true;
+            while(s<l){
+                if(str.charAt(s)!=str.charAt(l)){
+                    flag=false;
+                }
+                s++;
+                l--;
+            }
+            if(flag){
+                return str;
+            }
+        }return "";
+    }
+}
