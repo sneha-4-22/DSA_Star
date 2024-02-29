def4dc5a0d75268a515a085c3f120b3a52187b70
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-02-29 13:52:48 +0530

    Update EvenOdd.java

--- Trees/EvenOdd.java
+++ Trees/EvenOdd.java
@@ -1,4 +1,16 @@
 import java.util.*;
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+ }
 class EvenOdd {
     public boolean isEvenOddTree(TreeNode root) {
         if (root == null) {
