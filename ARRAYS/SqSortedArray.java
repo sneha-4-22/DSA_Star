fb8f48303de7f59233a05c3eaf3a6209bee58889
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-03-02 21:42:28 +0530

    Update SqSortedArray.java

--- ARRAYS/SqSortedArray.java
+++ ARRAYS/SqSortedArray.java
@@ -1,1 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
 
+}
