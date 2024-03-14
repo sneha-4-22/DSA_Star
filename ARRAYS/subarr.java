3af0eaee37f6fe9bf1eb00a1b55e9aa88feb06cd
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-03-14 17:54:12 +0530

    Update subarr.java

--- ARRAYS/subarr.java
+++ ARRAYS/subarr.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    private int atMost(int[] nums, int goal) {
+        int head, tail = 0, sum = 0, result = 0;
+        for (head = 0; head < nums.length; head++) {
+            sum += nums[head];
+            while (sum > goal && tail <= head) {
+                sum -= nums[tail];
+                tail++;
+            }
+            result += head - tail + 1;
+        }
+        return result;
+    }
+}
