56677e44fd2e32538d6fee64564a00529d9c06a4
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-03-19 21:40:01 +0530

    Update TaskScheduler.java

--- ARRAYS/TaskScheduler.java
+++ ARRAYS/TaskScheduler.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq=new int[26];
+        for(char c: tasks){
+            freq[c-'A']++;
+        }
+        Arrays.sort(freq);
+        int chunk=freq[25]-1;
+        int idle=chunk*n;
+        for(int i=24;i>=0;i--){
+            idle-=Math.min(chunk,freq[i]);
+        }return idle<0?tasks.length:tasks.length+idle;
+    }
+}
