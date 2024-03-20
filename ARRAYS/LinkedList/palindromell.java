8b7b973e555d02b351683ea4bfbcab568f69b786
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-03-20 20:52:26 +0530

    Update palindromell.java

--- ARRAYS/LinkedList/palindromell.java
+++ ARRAYS/LinkedList/palindromell.java
@@ -1,1 +1,25 @@
-
+class palindromell{
+  public boolean ispalindrome(ListNode head){
+    ListNode slow=ListNode(-1);
+    slow.next=head;
+    ListNode fast=head;
+    while(fast!=null&&fast.next!=null){
+      slow=slow.next;
+      fast=fast.next.next;
+    }
+    ListNode prev=null;
+    ListNode curr=slow.next;
+    while(curr!=null){
+      ListNode next=curr.next;
+      curr.next=prev;
+      prev=curr;
+      curr=next;
+    }
+    while(prev!=null){
+      if(head.val!=prev.val) return false;
+      prev=prev.next;
+      head=head.next;
+    }
+    return false;
+  } 
+}
