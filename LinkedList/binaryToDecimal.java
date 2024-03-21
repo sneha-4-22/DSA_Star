f55ed9464ff66a5d64368175a44536c6cd1c563b
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2024-03-21 19:05:25 +0530

    Update binaryToDecimal.java

--- LinkedList/binaryToDecimal.java
+++ LinkedList/binaryToDecimal.java
@@ -1,1 +1,9 @@
-
+class binaryToDecimal{
+  public int getDecimalValue(ListNode head){
+    int res=0;
+    while(head!=null){
+      res=2*res+head.val;
+      head=head.next;
+    }return res;
+  }
+}
